bbd501d2269c117bfb997b7a3fe9ec509c06b8e2
Richard A Burton <richardaburton@gmail.com>
2015-05-18 12:03:41 +0100

    upload of drivers, mutex and ntp code

---
+++ mutex/mutex.h
@@ -0,0 +1,17 @@
+//////////////////////////////////////////////////
+// Mutex support for ESP8266.
+// richardaburton@gmail.com
+//////////////////////////////////////////////////
+
+#ifndef __MUTEX_H__
+#define __MUTEX_H__
+
+#include <c_types.h>
+
+typedef int32 mutex_t;
+
+void ICACHE_FLASH_ATTR CreateMutux(mutex_t *mutex);
+bool ICACHE_FLASH_ATTR GetMutex(mutex_t *mutex);
+void ICACHE_FLASH_ATTR ReleaseMutex(mutex_t *mutex);
+
+#endif
